dd624517785aa875ab13a847d2a44aed9d3c884f
gichobih <dennismuri254@gmail.com>
2024-11-07 00:46:23 -0800

    filtered_logger.py/maim.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """
 Module for handling Personal Data
 """
+
 from typing import List
 import re
 import logging
@@ -11,11 +12,10 @@ import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """ Returns a log message obfuscated """
-    for f in fields:
-        message = re.sub(f'{f}=.*?{separator}',
-                         f'{f}={redaction}{separator}', message)
+    """Obfuscates specified fields in a log message."""
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
