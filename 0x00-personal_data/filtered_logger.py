d02887141ff0873461694c69daf09130ffc8a05f
gichobih <dennismuri254@gmail.com>
2024-11-07 00:31:05 -0800

    filtered_logger.py/maim.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """
-Module for handling and obfuscating sensitive personal data in log messages.
+Module for handling Personal Data
 """
 
 from typing import List
 import re
 
-
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """Returns log message with specified fields obfuscated"""
-    pattern = f"({'|'.join(map(re.escape, fields))})=.*?{re.escape(separator)}"
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscates specified fields in a log message."""
+    pattern = f"({'|'.join(fields)})=.*?{separator}"
     return re.sub(pattern, f"\\1={redaction}{separator}", message)
+
