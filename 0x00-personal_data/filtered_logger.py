711744ae356eca6b38b9ad3bd120248296da0f4f
gichobih <dennismuri254@gmail.com>
2024-11-07 00:39:29 -0800

    filtered_logger.py/maim.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,5 +7,7 @@ import re
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Obfuscates specified fields in a log message."""
-    pattern = f"({'|'.join(fields)})=.*?{separator}"
-    return re.sub(pattern, f"\\1={redaction}{separator}", message)
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
